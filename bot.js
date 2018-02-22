9fcd4b46604b44cdb13805068a5276f744b7a57a
webdastur <webdastur@umail.uz>
2018-02-22 08:44:47 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,5 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = 'Botingiz_TOKEnini_Shu_Yerga yozing';
+const bot = new TelegramBot(token, {polling: true});
+
+// Bu yerga o'zingizni botingiz kodlarini kiritasiz!
