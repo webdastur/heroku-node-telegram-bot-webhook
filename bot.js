1b54f2aece9889b3846feb7c922efc939e593013
webdastur <webdastur@umail.uz>
2018-02-23 08:02:16 +0400

    Update bot.js
    Botga examplar qo'shildi!

--- bot.js
+++ bot.js
@@ -3,3 +3,17 @@ const token = 'Botingiz_TOKEnini_Shu_Yerga yozing';
 const bot = new TelegramBot(token, {polling: true});
 
 // Bu yerga o'zingizni botingiz kodlarini kiritasiz!
+//Pastdagi Kodlarni o'chirib yuborishingiz mumkin.
+//botni ishga tushurish
+bot.onText(/\/start/, msg =>{
+    bot.sendMessage(msg.chat.id,'Salom botga hush kelibsiz ', msg.from.first_name)
+})
+//Rasm Jo'natish
+bot.onText(/\/photo/, msg =>{
+    bot.sendPhoto(msg.chat.id,'https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg',{
+        caption:'Izoh'
+    })
+})
+if ((msg.text !== '/start')||(msg.text !== '/photo')){
+    bot.sendMessage(msg.chat.id, msg.text)
+}
